71aae02fc7931766a7c5e11b91a9c691e375a138
seeu359 <a.ch3re359@yandex.ru>
2022-09-08 21:27:59 +0300

    Added new tests, new fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,14 @@ def _result_format_link():
 
 @pytest.fixture()
 def _fixture_hexlet_courses():
-    with open('tests/fixtures/hexlet-courses-fixture') as _fixture:
+    with open('tests/fixtures/hexlet-courses-fixture.html') as _fixture:
+        fixture = _fixture.read()
+    return fixture
+
+
+@pytest.fixture()
+def _fixture_picture():
+    with open('tests/fixtures/picture_from_html.png', 'rb') as _fixture:
         fixture = _fixture.read()
     return fixture
 
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -5,11 +5,13 @@ import tempfile
 import os
 
 
+TEST_LINK = 'http://test.com'
+
+
 def test_get_link_data():
     with requests_mock.Mocker() as mock:
-        test_link = 'http://test.com'
-        mock.get(test_link, text='mock_test')
-        result = page_loader.get_link_data(test_link)
+        mock.get(TEST_LINK, text='mock_test')
+        result = page_loader.get_link_data(TEST_LINK)
         assert result == 'mock_test'
 
 
@@ -18,14 +20,25 @@ def test_get_format_path(_result_format_link):
     assert result == _result_format_link
 
 
-def test_download(_fixture_hexlet_courses):
+def test_download_page(_fixture_hexlet_courses):
     with tempfile.TemporaryDirectory() as tmp:
         with requests_mock.Mocker() as mock:
-            test_link = 'http://test.com'
             path = pathlib.Path(tmp, 'test-com.html')
-            mock.get(test_link, text=_fixture_hexlet_courses)
-            page_loader.download(test_link, tmp)
+            mock.get(TEST_LINK, text=_fixture_hexlet_courses)
+            page_loader.download(TEST_LINK, tmp)
         with open(path) as test_f:
             test_file = test_f.read()
         assert os.path.isfile(path) is True
         assert test_file == _fixture_hexlet_courses
+
+
+def test_download_picture(_fixture_picture):
+    with tempfile.TemporaryDirectory() as tmp:
+        webpage_path = 'tests/fixtures/hexlet-courses-fixture.html'
+        page_loader.download_pictures(webpage_path, tmp, TEST_LINK)
+        for i in os.listdir(tmp):
+            path = pathlib.Path(tmp, i)
+            with open(path, 'rb') as test_pic:
+                test_picture = test_pic.read()
+        assert test_picture == _fixture_picture
+        assert len(os.listdir(tmp)) == 1
